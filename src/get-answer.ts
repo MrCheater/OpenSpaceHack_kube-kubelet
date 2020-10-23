b3947b024f963563b3a3d499d970335b0a21a237
mrcheater <slayerx@narod.ru>
2020-10-23 23:59:51 +0300

    Initial

---
+++ src/get-answer.ts
@@ -0,0 +1,8 @@
+const getAnswer = (question: string): string => {
+  let answer =
+    "Я не могу ответить на ваш вопрос. Попробуйте его переформулировать";
+
+  return answer;
+};
+
+export default getAnswer;
---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+import getAnswer from "./get-answer";
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+/*
+GET /api/chat/v1/bot?question=<some question>
+  { "answer": "<some answer>" }
+*/
+
+app.get("/api/chat/v1/bot", (req, res) => {
+  const { question } = req.query;
+
+  const answer = getAnswer(question);
+
+  res.json({
+    answer,
+  });
+});
+
+app.listen(3000, () => {
+  console.log(
+    `Example app listening at http://localhost:3000/api/chat/v1/bot?question=`
+  );
+});
